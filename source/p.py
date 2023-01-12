471d5addd482edb2cc8076b22eedcd4264348c91
Jens Heine <binbash@gmx.net>
2023-01-12 10:01:48 +0100

    pshell: cplast new, bugfix when reusing last typed command after retyping passwd

--- source/p.py
+++ source/p.py
@@ -19,7 +19,7 @@ colorama.init()
 #
 # VARIABLES
 #
-VERSION = "p by Jens Heine <binbash@gmx.net> version: 2023.01.11"
+VERSION = "p by Jens Heine <binbash@gmx.net> version: 2023.01.12"
 database_filename = 'p.db'
 TEMP_MERGE_DATABASE_FILENAME = "temp_dropbox_p.db"
 
