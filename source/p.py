a340d4ef8bc6f2d383c191ba28935ec10acc4273
binbash23 <binbash@gmx.net>
2023-01-30 20:54:44 +0100

    execute sql fixed

--- source/p.py
+++ source/p.py
@@ -20,7 +20,7 @@ colorama.init()
 #
 # VARIABLES
 #
-VERSION = "p by Jens Heine <binbash@gmx.net> version: 2023.01.27"
+VERSION = "p by Jens Heine <binbash@gmx.net> version: 2023.01.30"
 database_filename = 'p.db'
 TEMP_MERGE_DATABASE_FILENAME = "temp_dropbox_p.db"
 
