b35cfc5ed6ebab9586a051b18d4aa23eb08b7f6d
Jens Heine <binbash@gmx.net>
2023-08-16 11:47:11 +0200

    showconfig also with print_slow

--- source/p.py
+++ source/p.py
@@ -19,7 +19,7 @@ colorama.init()
 #
 # VARIABLES
 #
-VERSION = "[p] by Jens Heine <binbash@gmx.net> version: 2023.08.13"
+VERSION = "[p] by Jens Heine <binbash@gmx.net> version: 2023.08.16"
 database_filename = 'p.db'
 TEMP_MERGE_DATABASE_FILENAME = "temp_dropbox_p.db"
 URL_DOWNLOAD_BINARY_P_WIN = "https://github.com/binbash23/p/raw/master/dist/windows/p.exe"
