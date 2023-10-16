bddcf87f0e6f96f31a5bfb2e159c59c3196dcb6c
binbash23 <binbash@gmx.net>
2023-10-16 14:14:06 +0200

    implementation of new connector stuff (webdav) and code cleanup

--- source/p.py
+++ source/p.py
@@ -20,7 +20,7 @@ colorama.init()
 #
 # VARIABLES
 #
-VERSION = "[p] by Jens Heine <binbash@gmx.net> version: 2023.10.08"
+VERSION = "[p] by Jens Heine <binbash@gmx.net> version: 2023.10.16"
 database_filename = 'p.db'
 TEMP_MERGE_DATABASE_FILENAME = "temp_merge_database.db"
 URL_GITHUB_P_HOME = "https://github.com/binbash23/p"
