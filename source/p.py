1c413a283a829b24f2d0b1b46ceca2cd44bdd5bd
Jens Heine <binbash@gmx.net>
2024-01-28 21:06:40 +0100

    warning to not merge an unencrypted db

--- source/p.py
+++ source/p.py
@@ -16,7 +16,7 @@ colorama.init()
 #
 # VARIABLES
 #
-VERSION = "[p] by Jens Heine <binbash@gmx.net> version: 2024.01.23"
+VERSION = "[p] by Jens Heine <binbash@gmx.net> version: 2024.01.28"
 database_filename = 'p.db'
 URL_GITHUB_P_HOME = "https://github.com/binbash23/p"
 URL_GITHUB_P_WIKI = "https://github.com/binbash23/p/wiki"
