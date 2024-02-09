e931e9f0375da2c7abebcc196568d080cf3aa695
Jens Heine <binbash@gmx.net>
2024-02-10 00:55:29 +0100

    arm architecture detection for raspberry pi update

--- source/p.py
+++ source/p.py
@@ -16,7 +16,7 @@ colorama.init()
 #
 # VARIABLES
 #
-VERSION = "[p] by Jens Heine <binbash@gmx.net> version: 2024.02.06"
+VERSION = "[p] by Jens Heine <binbash@gmx.net> version: 2024.02.10"
 database_filename = 'p.db'
 URL_GITHUB_P_HOME = "https://github.com/binbash23/p"
 URL_GITHUB_P_WIKI = "https://github.com/binbash23/p/wiki"
