896a2c746e52197c57eaad8b699cc8909c1cf996
Jens Heine <jens.heine@stoelting-gruppe.de>
2024-02-25 11:56:40 +0100

    windows binary

--- source/create_binary_and_push.ps1
+++ source/create_binary_and_push.ps1
@@ -1,12 +1,20 @@
+
+
+git pull
+
 pyinstaller p.py --onefile --bootloader-ignore-signals --log-level=WARN
 
 cp dist\p.exe ..\dist\windows\p.exe
 
-#git add -f ..\dist\windows\p.exe
+git add -f ..\dist\windows\p.exe
 
-#git status
+git status
 
-#git commit -m "New windows binary"
+read-host "Press enter to git commit..."
 
-#git push
+git commit -m "New windows binary"
+
+read-host "Press enter to git push..."
+
+git push
 
