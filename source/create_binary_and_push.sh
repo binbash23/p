6b101ece6c793bd59338f7d886da436e8cd36c95
Jens Heine <binbash@gmx.net>
2024-02-25 11:40:09 +0100

    raspberry folder renamed to arm64, new create script for arm

--- source/create_binary_and_push.sh
+++ source/create_binary_and_push.sh
@@ -4,6 +4,8 @@
 #
 set -e
 
+git pull
+
 #pyinstaller p.py --onefile --bootloader-ignore-signals --log-level=WARN --clean
 pyinstaller p.py --onefile --bootloader-ignore-signals --clean
 
@@ -20,4 +22,6 @@ git commit -m "linux binary"
 read -p "Press enter to git push..."
 
 git push
+
+git status
 
