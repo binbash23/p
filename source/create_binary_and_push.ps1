c9de559a37b71eab9c3302d4394db82c579ccc2e
Jens Heine <jens.heine@stoelting-gruppe.de>
2024-03-12 20:03:26 +0100

    New windows binary

--- source/create_binary_and_push.ps1
+++ source/create_binary_and_push.ps1
@@ -4,9 +4,9 @@ git pull
 
 pyinstaller p.py --onefile --bootloader-ignore-signals --log-level=WARN
 
-cp dist\p.exe ..\dist\windows\p.exe
+cp dist/p.exe ../dist/windows/p.exe
 
-git add -f ..\dist\windows\p.exe
+git add -f ../dist/windows/p.exe
 
 git status
 
